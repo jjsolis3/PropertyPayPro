210f3688566f72640016d305cd0ac0548bd90508
Jose J Solis <jjsolis3@asu.edu>
2026-06-20 08:47:32 +0000

    Add migration for Phases 2-3

---
+++ src/PropertyPayPro/Migrations/20260620084701_Phases2and3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PropertyPayPro.Migrations
+{
+    /// <inheritdoc />
+    public partial class Phases2and3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
