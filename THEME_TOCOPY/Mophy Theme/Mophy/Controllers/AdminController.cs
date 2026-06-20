9dfeb7e316adde822f37adcb37afbc6be0d91376
Jose J Solis <jjsolis3@asu.edu>
2026-06-20 01:03:46 -0700

    Web Application Theme Ideas - Reference
    Added Theme for Reference/Ideas

---
+++ THEME_TOCOPY/Mophy Theme/Mophy/Controllers/AdminController.cs
@@ -0,0 +1,326 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Mophy.Controllers
+{
+	public class AdminController : Controller
+	{
+		public IActionResult AddBlog()
+		{
+			return View();
+		}
+		public IActionResult AddEmail()
+		{
+			return View();
+		}
+		public IActionResult AppCalender()
+		{
+			return View();
+		}
+		public IActionResult AppProfile()
+		{
+			return View();
+		}
+		public IActionResult Blog()
+		{
+			return View();
+		}
+		public IActionResult BlogCategory()
+		{
+			return View();
+		}
+		public IActionResult CardsCenter()
+		{
+			return View();
+		}
+		public IActionResult ChartChartist()
+		{
+			return View();
+		}
+		public IActionResult ChartChartjs()
+		{
+			return View();
+		}
+		public IActionResult ChartFlot()
+		{
+			return View();
+		}
+		public IActionResult ChartMorris()
+		{
+			return View();
+		}
+		public IActionResult ChartPeity()
+		{
+			return View();
+		}
+		public IActionResult ChartSparkline()
+		{
+			return View();
+		}
+		public IActionResult Content()
+		{
+			return View();
+		}
+		public IActionResult ContentAdd()
+		{
+			return View();
+		}
+		public IActionResult EcomCheckout()
+		{
+			return View();
+		}
+		public IActionResult EcomCustomers()
+		{
+			return View();
+		}
+		public IActionResult EcomInvoice()
+		{
+			return View();
+		}
+		public IActionResult EcomProductDetail()
+		{
+			return View();
+		}
+		public IActionResult EcomProductGrid()
+		{
+			return View();
+		}
+		public IActionResult EcomProductList()
+		{
+			return View();
+		}
+		public IActionResult EcomProductOrder()
+		{
+			return View();
+		}
+		public IActionResult EditProfile()
+		{
+			return View();
+		}
+		public IActionResult EmailCompose()
+		{
+			return View();
+		}
+		public IActionResult EmailInbox()
+		{
+			return View();
+		}
+		public IActionResult EmailRead()
+		{
+			return View();
+		}
+		public IActionResult EmailTemplate()
+		{
+			return View();
+		}
+		public IActionResult EmptyPage()
+		{
+			return View();
+		}
+		public IActionResult FormCkeditor()
+		{
+			return View();
+		}
+		public IActionResult FormElement()
+		{
+			return View();
+		}
+		public IActionResult FormPickers()
+		{
+			return View();
+		}
+		public IActionResult FormValidationJquery()
+		{
+			return View();
+		}
+		public IActionResult FormWizard()
+		{
+			return View();
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Index2()
+		{
+			return View();
+		}
+		public IActionResult Invoices()
+		{
+			return View();
+		}
+		public IActionResult MapJqvmap()
+		{
+			return View();
+		}
+		public IActionResult Menu()
+		{
+			return View();
+		}
+		public IActionResult MyWallet()
+		{
+			return View();
+		}
+		public IActionResult PageError400()
+		{
+			return View();
+		}
+		public IActionResult PageError403()
+		{
+			return View();
+		}
+		public IActionResult PageError404()
+		{
+			return View();
+		}
+		public IActionResult PageError500()
+		{
+			return View();
+		}
+		public IActionResult PageError503()
+		{
+			return View();
+		}
+		public IActionResult PageForgotPassword()
+		{
+			return View();
+		}
+		public IActionResult PageLockScreen()
+		{
+			return View();
+		}
+		public IActionResult PageLogin()
+		{
+			return View();
+		}
+		public IActionResult PageRegister()
+		{
+			return View();
+		}
+		public IActionResult PostDetails()
+		{
+			return View();
+		}
+		public IActionResult TableBootstrapBasic()
+		{
+			return View();
+		}
+
+		public IActionResult TableDatatableBasic()
+		{
+			return View();
+		}
+		public IActionResult Transactions()
+		{
+			return View();
+		}
+		public IActionResult TransactionsDetails()
+		{
+			return View();
+		}
+		public IActionResult UcLightgallery()
+		{
+			return View();
+		}
+		public IActionResult UcNestable()
+		{
+			return View();
+		}
+		public IActionResult UcNouiSlider()
+		{
+			return View();
+		}
+		public IActionResult UcSelect2()
+		{
+			return View();
+		}
+		public IActionResult UcSweetalert()
+		{
+			return View();
+		}
+		public IActionResult UcToastr()
+		{
+			return View();
+		}
+		public IActionResult UiAccordion()
+		{
+			return View();
+		}
+		public IActionResult UiAlert()
+		{
+			return View();
+		}
+		public IActionResult UiBadge()
+		{
+			return View();
+		}
+		public IActionResult UiButton()
+		{
+			return View();
+		}
+		public IActionResult UiButtonGroup()
+		{
+			return View();
+		}
+		public IActionResult UiCard()
+		{
+			return View();
+		}
+		public IActionResult UiCarousel()
+		{
+			return View();
+		}
+		public IActionResult UiDropdown()
+		{
+			return View();
+		}
+		public IActionResult UiGrid()
+		{
+			return View();
+		}
+		public IActionResult UiListGroup()
+		{
+			return View();
+		}
+		public IActionResult UiMediaObject()
+		{
+			return View();
+		}
+		public IActionResult UiModal()
+		{
+			return View();
+		}
+		public IActionResult UiPagination()
+		{
+			return View();
+		}
+		public IActionResult UiPopover()
+		{
+			return View();
+		}
+		public IActionResult UiProgressbar()
+		{
+			return View();
+		}
+		public IActionResult UiTab()
+		{
+			return View();
+		}
+		public IActionResult UiTypography()
+		{
+			return View();
+		}
+		public IActionResult WidgetCard()
+		{
+			return View();
+		}
+		public IActionResult WidgetChart()
+		{
+			return View();
+		}
+		public IActionResult WidgetList()
+		{
+			return View();
+		}
+	}
+}
---
+++ THEME_TOCOPY/Mophy Theme/Mophy/Controllers/FrontendController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mophy.Controllers
+{
+	public class FrontendController : Controller
+	{
+		public IActionResult AboutUs()
+		{
+			return View();
+		}
+		public IActionResult BlogDetails()
+		{
+			return View();
+		}
+		public IActionResult BlogGrid()
+		{
+			return View();
+		}
+		public IActionResult BlogList()
+		{
+			return View();
+		}
+		public IActionResult ContactUs()
+		{
+			return View();
+		}
+		public IActionResult FAQ()
+		{
+			return View();
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Pricing()
+		{
+			return View();
+		}
+	}
+}
