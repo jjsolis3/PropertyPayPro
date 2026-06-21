b253ca6394be3d50999efd18d9e6d4ce7605c06a
Jose Solis <jjsolis3@asu.edu>
2026-06-21 08:20:40 +0000

    Add EmailAndTenantPrefs migration

---
+++ src/PropertyPayPro/Migrations/20260621082040_EmailAndTenantPrefs.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PropertyPayPro.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmailAndTenantPrefs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
