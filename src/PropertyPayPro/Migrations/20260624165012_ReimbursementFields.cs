aabce1929ec8bc2f0bcd15a0d0e87397fe1491ef
Jose Solis <jjsolis3@asu.edu>
2026-06-24 17:57:29 +0000

    Add ReimbursementFields migration

---
+++ src/PropertyPayPro/Migrations/20260624165012_ReimbursementFields.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PropertyPayPro.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReimbursementFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ReimbursedAmount",
+                table: "PropertyExpenses",
+                type: "numeric(10,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "ReimbursedOn",
+                table: "PropertyExpenses",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReimbursedAmount",
+                table: "PropertyExpenses");
+
+            migrationBuilder.DropColumn(
+                name: "ReimbursedOn",
+                table: "PropertyExpenses");
+        }
+    }
+}
