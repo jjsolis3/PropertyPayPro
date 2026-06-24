64a1ca89c437521c051258d9eed0d479b89fa59f
Jose Solis <jjsolis3@asu.edu>
2026-06-24 16:42:49 +0000

    Add PaidByTenant migration

---
+++ src/PropertyPayPro/Migrations/20260624164205_PaidByTenant.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PropertyPayPro.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaidByTenant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PaidByTenantId",
+                table: "RentPayments",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RentPayments_PaidByTenantId",
+                table: "RentPayments",
+                column: "PaidByTenantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RentPayments_Tenants_PaidByTenantId",
+                table: "RentPayments",
+                column: "PaidByTenantId",
+                principalTable: "Tenants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RentPayments_Tenants_PaidByTenantId",
+                table: "RentPayments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RentPayments_PaidByTenantId",
+                table: "RentPayments");
+
+            migrationBuilder.DropColumn(
+                name: "PaidByTenantId",
+                table: "RentPayments");
+        }
+    }
+}
